aeabc31322e147b5061d59644053eea5567685b0
sasasan03 <sako.hiro62@gmail.com>
2023-08-03 22:53:53 +0900

    [add]: タプルを使用したプロトコルのサンプルコードを追加

--- IndependentActivitySampleAPP/Protocol.playground/Contents.swift
+++ IndependentActivitySampleAPP/Protocol.playground/Contents.swift
@@ -52,7 +52,7 @@ struct VectorInt: TransVector, CustomStringConvertible {
     typealias Element = Int
     var x: Int
     var y: Int
-    
+
     func transposed() -> VectorInt {
         return VectorInt(x: self.y, y: self.x)
     }
@@ -61,14 +61,59 @@ struct VectorInt: TransVector, CustomStringConvertible {
     }
     var description: String{ return "[\(x),\(y)]"}
 }
+//
+//let a = VectorInt(x: 10, y: 3)
+//print(a)
+//print(a.transposed())
+//print(a)
+//let b = VectorInt(x: -1, y: 6)
+////❓❓+はどうしてインスタンス化しなくても使えてん？
+////🍔二項演算しとして認識している。
+//print(a + b)
 
-let a = VectorInt(x: 10, y: 3)
-print(a)
-print(a.transposed())
-print(a)
-let b = VectorInt(x: -1, y: 6)
-//❓❓+はどうしてインスタンス化しなくても使えてん？
-//🍔二項演算しとして認識している。
-print(a + b)
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🍹P126
 
+protocol EqVector: Equatable {
+    associatedtype Element: Equatable
+    var x: Element { get set }
+    var y: Element { get set }
+}
 
+struct LabeledPoint: EqVector, CustomStringConvertible{
+    typealias Element = Int
+    var label: String
+    var x: Int
+    var y: Int
+    var description: String { "[\(x),\(y)]" }
+}
+
+struct ShopOnMap: EqVector, CustomStringConvertible{
+    var shop: (name: String, comment: String?)
+    var x, y: Float
+    init(_ s: String, N: Float, E: Float, comment: String? ){
+        self.shop = (name: s, comment: comment)
+        self.x = N
+        self.y = E
+    }
+    var description: String {
+        var r = shop.name + "北緯\(self.x)経緯\(self.y)にあります"
+        if let msg = shop.comment {
+            r += " " + msg
+        }
+        return r
+    }
+    static func == (lhs: ShopOnMap, rhs: ShopOnMap) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
+    }
+}
+
+//let mx:VectorInt.Element = 10
+//let a = LabeledPoint(label: "A", x: mx, y: 7)
+//var b = LabeledPoint(label: "B", x: 10, y: mx - 3)
+//print(a == b)
+//b.label = "A"
+//print(a == b)
+
+let shop01 = ShopOnMap("あさだ", N: 300.1, E: 200.1, comment: "文房具屋")
+let shop02 = ShopOnMap("朝田", N: 300.1, E: 200.1, comment: "ジャンプ売ってる")
+print(shop01 == shop02)
