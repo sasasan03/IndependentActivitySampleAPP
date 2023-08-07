34870d5ee56c8fc99b7c3edb6b182de9b43cbb1a
sasasan03 <sako.hiro62@gmail.com>
2023-08-07 16:51:31 +0900

    [add]: Setの比較の処理を追加

--- IndependentActivitySampleAPP/OptionSet.playground/Contents.swift
+++ IndependentActivitySampleAPP/OptionSet.playground/Contents.swift
@@ -1,44 +1,137 @@
 import Foundation
 
 //🟥Setの実験
-//🍔数字
+////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🍔p268   集合の型と初期値
+//var s: Set<String>
+//s = ["フランダース", "フランドル", "フランシスカ"]
+//var e = Set<Int>()
+//var t = Set<Int>(0..<8)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>シーケンス型を引数にできるだと！？
+//let drinks = Set<String>(["bear","wine","sake"])
+//print(drinks)
+
+////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🍔p269   要素へのアクセス
 //let nums =  Set(0..<8)
 //for num in nums {
 //    print(num, terminator: "　")
 //}
 //nums.first//3を出力
 
-//🍔文字
+////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>文字
 //let fruits: Set<String> = ["remon", "apple", "grap", "peach"]
 //for fruit in fruits {
 //    print(fruit, terminator:  "　")
 //}
 
-//🍔重複を避ける
-//let alphabets: Set<String> = ["a","b","a","c","d"]
-//for alp in alphabets {
-//    print(alp, terminator: "　")
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🍔p270  タプルを含まないデータモデル型
+//struct Person: Hashable {
+//    let name: String
+//    let age: Int
+//}
+//
+//let persons: Set<Person> = [
+//    Person(name: "ひろ", age: 31),
+//    Person(name: "ひろ", age: 31),
+//    Person(name: "まこ", age: 30),
+//    Person(name: "ままこ", age: 30),
+//    Person(name: "まさ", age: 29),
+//    Person(name: "まさ", age: 28)
+//
+//]
+//for person in persons {
+//    print(person, terminator: "🍹")
 //}
 
-//🍔データモデル型はどうなの？
-struct Person: Hashable {
-    let name: String
-    let age: Int
-}
+//================================🍹まとめて渡したいときに使用by yamadaさん
+//let test1 = 1 //Aから渡ってくる
+//let test2 = "a"//Bから渡ってくる
+//let test3: TestEnum = .hogehoge
+//let tuple = (test1, test2, test3)
 
-let persons: Set<Person> = [
-    Person(name: "ひろ", age: 31),
-    Person(name: "ひろ", age: 31),
-    Person(name: "まこ", age: 30),
-    Person(name: "ままこ", age: 30),
-    Person(name: "まさ", age: 29),
-    Person(name: "まさ", age: 28)
-    
-]
-for person in persons {
-    print(person, terminator: "🍹")
-}
+//=================================
 
-//🍔シーケンス型を引数にできるだと！？
-//let drinks = Set<String>(["bear","wine","sake"])
-//print(drinks)
+//struct Person: Hashable, CustomStringConvertible {
+//    enum Sex: String { case male = "M", female = "F" }
+//    let name: String
+//    var attr: (age: Int, sex: Sex) //ハッシャブルに適合していない
+//    init(_ name: String, _ age: Int, _ sex: Sex){
+//        self.name = name
+//        self.attr = (age: age ,sex: sex)
+//    }
+//    static func ==(lhs: Person, rhs: Person) -> Bool {
+//        return lhs.attr == rhs.attr && lhs.name == rhs.name
+//    }
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(name)
+//        hasher.combine(attr.age)
+//    }
+//    var description: String {
+//        name + "🍔歳は『\(attr.age)』,性別は『\(attr.sex.rawValue)』🍹"
+//    }
+//}
+//
+//let sako = Person("sako", 31, Person.Sex.male)
+//let mako = Person("mako", 31, Person.Sex.male)
+//let maho = Person("maho", 31, Person.Sex.female)
+//let hiro = Person("sako", 31, Person.Sex.male)
+//let ryoko = Person("ryoko", 29, Person.Sex.female)
+//let personArray:Set<Person> = [
+//    sako,mako,maho,hiro,ryoko
+//]
+//print(personArray)
+//---------------------------------------------------------型同士の比較(オリジナル)
+//試しに自分で作ったやつ
+//struct Human: Equatable,Hashable {
+//    enum Sex: String { case male = "M", femal = "F"}
+//    var name: String
+//    var attr: (age: Int, sex: Sex)
+//    static func ==(lhs: Human, rhs: Human) -> Bool{
+//        return lhs.name == rhs.name || lhs.attr == rhs.attr
+//    }
+//    //🍔エラー：イミュータブルな値に対してミューティング・メンバーを使用できない： 'hasher' は 'let' 定数である。
+////    func hash(hasher: Hasher) {
+////        hasher.combine(name)
+////        hasher.combine(attr.age)
+////    }
+//    func hash(into hasher: inout Hasher) {//これで解決。ただintoの役割がわからん。Hasher型に関係ありか？
+//        hasher.combine(name)
+//        hasher.combine(attr.age)
+//    }
+//}
+////
+////let sako = Human(name: "sako", attr: (age: 31, sex: Human.Sex.male))
+////let mako = Human(name: "mako", attr: (age: 31, sex: Human.Sex.male))
+////print(sako == mako)
+//
+////var personSet:Set<Human> = [
+////    Human(name: "🍔sako", attr: (age: 30, sex: Human.Sex.male)),
+////    Human(name: "🍹mako", attr: (age: 31, sex: Human.Sex.male)),
+////    Human(name: "🍟masa", attr: (age: 31, sex: Human.Sex.male)),
+////    Human(name: "🍟masa", attr: (age: 31, sex: Human.Sex.male))
+////]
+////print(personSet)
+//var array: Set<String> = ["🍇","🍎","🍋","😀"]
+//let human = ["😃","😀","🙃",]
+////personSet.union(array)
+////集合に、other にも含まれる要素がすでに 1 つ以上ある場合は、既存のメンバが保持される。other に等価な要素の複数のインスタンスが含まれる場合は、最初のインスタンスのみが保持されます。
+//array.formUnion(human)
+//array.union(human)
+//print(array)
+//---------------------------------------------------------🍔p271　集合演算
+//var array:Set<String> = Set.init()//初期化[]
+//array.isEmpty//true
+//let furits02 = ["もも","なし","ライチ"]
+//let fruits = ["りんご","もも","なし"]
+//var array2 = array.union(fruits)
+//
+//print(array)
+//print(array2)//["りんご","なし","もも"]
+//array2.contains("りんご")//true
+//array2.remove("りんご")
+//print(array2)//["なし", "もも"]
+//array2.isSubset(of: furits02)//true
+////🍔array2の["なし", "もも"]と全く同じ要素を持っているか、
+////     ["なし", "もも"]＋他の持っていない要素を持っている場合にtrueを返す。
+//----------------------------------------
+
--- IndependentActivitySampleAPP/Protocol.playground/Contents.swift
+++ IndependentActivitySampleAPP/Protocol.playground/Contents.swift
@@ -40,80 +40,130 @@ import UIKit
 //print(g)
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🍹P124
-protocol TransVector {
-    associatedtype Element
-    var x: Element { get set }
-    var y: Element { get set }
-    func transposed() -> Self
-    static func +(lhs: Self, rhs: Self) -> Self
-}
-
-struct VectorInt: TransVector, CustomStringConvertible {
-    typealias Element = Int
-    var x: Int
-    var y: Int
-
-    func transposed() -> VectorInt {
-        return VectorInt(x: self.y, y: self.x)
-    }
-    static func +(lhs: VectorInt, rhs: VectorInt) -> VectorInt {
-        return VectorInt(x: lhs.x + rhs.x, y: lhs.x + rhs.y)
-    }
-    var description: String{ return "[\(x),\(y)]"}
-}
+//protocol TransVector {
+//    associatedtype Element
+//    var x: Element { get set }
+//    var y: Element { get set }
+//    func transposed() -> Self
+//    static func +(lhs: Self, rhs: Self) -> Self
+//}
+//
+//struct VectorInt: TransVector, CustomStringConvertible {
+//    typealias Element = Int
+//    var x: Int
+//    var y: Int
+//
+//    func transposed() -> VectorInt {
+//        return VectorInt(x: self.y, y: self.x)
+//    }
+//    static func +(lhs: VectorInt, rhs: VectorInt) -> VectorInt {
+//        return VectorInt(x: lhs.x + rhs.x, y: lhs.x + rhs.y)
+//    }
+//    var description: String{ return "[\(x),\(y)]"}
+//}
 //
 //let a = VectorInt(x: 10, y: 3)
 //print(a)
 //print(a.transposed())
 //print(a)
 //let b = VectorInt(x: -1, y: 6)
-////❓❓+はどうしてインスタンス化しなくても使えてん？
-////🍔二項演算しとして認識している。
+//❓❓+はどうしてインスタンス化しなくても使えてん？
+//🍔二項演算しとして認識している。
 //print(a + b)
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🍹P126
-
-protocol EqVector: Equatable {
-    associatedtype Element: Equatable
-    var x: Element { get set }
-    var y: Element { get set }
-}
-
-struct LabeledPoint: EqVector, CustomStringConvertible{
-    typealias Element = Int
-    var label: String
-    var x: Int
-    var y: Int
-    var description: String { "[\(x),\(y)]" }
-}
-
-struct ShopOnMap: EqVector, CustomStringConvertible{
-    var shop: (name: String, comment: String?)
-    var x, y: Float
-    init(_ s: String, N: Float, E: Float, comment: String? ){
-        self.shop = (name: s, comment: comment)
-        self.x = N
-        self.y = E
-    }
-    var description: String {
-        var r = shop.name + "北緯\(self.x)経緯\(self.y)にあります"
-        if let msg = shop.comment {
-            r += " " + msg
-        }
-        return r
-    }
-    static func == (lhs: ShopOnMap, rhs: ShopOnMap) -> Bool {
-        return lhs.x == rhs.x && lhs.y == rhs.y
-    }
-}
-
+//
+////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🍹P126
+//
+//protocol EqVector: Equatable {
+//    associatedtype Element: Equatable
+//    var x: Element { get set }
+//    var y: Element { get set }
+//}
+//
+//struct LabeledPoint: EqVector, CustomStringConvertible{
+//    typealias Element = Int
+//    var label: String
+//    var x: Int
+//    var y: Int
+//    var description: String { "[\(x),\(y)]" }
+//}
+//
+//struct ShopOnMap: EqVector, CustomStringConvertible{
+//    var shop: (name: String, comment: String?)
+//    var x, y: Float
+//    init(_ s: String, N: Float, E: Float, comment: String? ){
+//        self.shop = (name: s, comment: comment)
+//        self.x = N
+//        self.y = E
+//    }
+//    var description: String {
+//        var r = shop.name + "北緯\(self.x)経緯\(self.y)にあります"
+//        if let msg = shop.comment {
+//            r += " " + msg
+//        }
+//        return r
+//    }
+//    static func == (lhs: ShopOnMap, rhs: ShopOnMap) -> Bool {
+//        return lhs.x == rhs.x && lhs.y == rhs.y //&&  lhs.shop == rhs.shop
+//    }
+//}
+//
 //let mx:VectorInt.Element = 10
 //let a = LabeledPoint(label: "A", x: mx, y: 7)
 //var b = LabeledPoint(label: "B", x: 10, y: mx - 3)
 //print(a == b)
 //b.label = "A"
 //print(a == b)
+//
+//let shop01 = ShopOnMap("あさだ", N: 300.1, E: 200.1, comment: "文房具屋")
+//let shop02 = ShopOnMap("朝田", N: 300.1, E: 200.1, comment: "ジャンプ売ってる")
+//print(shop01 == shop02)
 
-let shop01 = ShopOnMap("あさだ", N: 300.1, E: 200.1, comment: "文房具屋")
-let shop02 = ShopOnMap("朝田", N: 300.1, E: 200.1, comment: "ジャンプ売ってる")
-print(shop01 == shop02)
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🍹P129
+//struct Time: Comparable, CustomStringConvertible{
+//
+//    let hour, min: Int
+//
+//    static func == (lhs: Time, rhs: Time) -> Bool{
+//        return lhs.hour == rhs.hour && lhs.min == rhs.min
+//    }
+//
+//    static func < (lhs: Time, rhs: Time) -> Bool {
+//        return lhs.hour < rhs.hour || (lhs.hour == rhs.hour && lhs.min < rhs.min)
+//    }
+//
+//    var description: String {
+//        let h = hour < 10 ? "\(hour)" : "\(hour)"
+//        let m = min < 10 ? "0\(min)" : "\(min)"
+//        return h + ":" + m
+//    }
+//}
+//
+//let t1 = Time(hour: 5, min: 00)
+//let t2 = Time(hour: 4, min: 10)
+//let t3 = Time(hour: 5, min: 3)
+//print(t1 == t2)
+//print(t3 != t2)
+//print(t2 > t1)
+//print([t1,t2,t3].sorted())
+//
+//struct PersonName: Comparable {
+//    static func < (lhs: PersonName, rhs: PersonName) -> Bool {
+//        return lhs.name.count > rhs.name.count
+//    }
+//    static func == (lhs: PersonName, rhs: PersonName) -> Bool {
+//        return lhs.name == rhs.name
+//    }
+//
+//    var name: String
+//}
+//
+//let sako = PersonName(name: "sako")
+//let mako = PersonName(name: "makoto")
+//print(sako < mako)
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>🍹P129
+//let array = ["a","b","c","d"]
+//print(array)
+//let arraySlice = array.dropFirst()
+//print(arraySlice)
+
